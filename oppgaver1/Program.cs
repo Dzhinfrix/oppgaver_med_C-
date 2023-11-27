8e7dae96ad978f7de09185ccd94cbbfbb61bf7ca
Alyona <fairy.albert.hofmann@gmail.com>
2023-11-27 09:57:53 +0100

    first task

---
+++ oppgaver1/Program.cs
@@ -0,0 +1,15 @@
+﻿
+int a = 35;
+int b = 5;
+string result;
+if (a == b * b)
+{
+    result = "yes";
+}
+else 
+{
+    result = "no";
+}
+Console.Write(result);
+// является ли число а квадратом числа b
+
