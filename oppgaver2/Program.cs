0d1d5971baec4f68a02521cbbd12e0bb0e5ff232
Alyona <fairy.albert.hofmann@gmail.com>
2023-11-27 10:00:08 +0100

    second task

---
+++ oppgaver2/Program.cs
@@ -0,0 +1,13 @@
+﻿int numberA = 4;
+int start = numberA * -1;
+int count = start;
+
+while (count <= numberA) {
+    Console.Write(count + " ");
+    count ++;
+
+}
+
+//count ++ = плюс 1
+// программа должна написать числа от -4 до 4
+// закончила на 1/39 семинар 1 
