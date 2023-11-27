e3a659d67fb9dd95a781bdf987154f9ca329c01e
Alyona <fairy.albert.hofmann@gmail.com>
2023-11-27 19:50:06 +0100

    task 4

---
+++ oppgaver4/Program.cs
@@ -0,0 +1,24 @@
+﻿int a = 3;
+int b = 1;
+int c = 2;
+string max;
+
+if (a > b) {
+    max = "a";
+}
+else {
+    max = "b";
+}
+if (b > c) {
+    max = "b";
+}
+else {
+    max = "c";
+}
+if (c > a) {
+    max = "c";
+}
+else {
+    max = "a";
+}
+Console.Write(max);
