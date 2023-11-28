4135b793af5ef660aa0f8e95e9d4bcdb3684dd83
Alyona <fairy.albert.hofmann@gmail.com>
2023-11-28 17:42:12 +0100

    task 5

---
+++ oppgaver5/Program.cs
@@ -0,0 +1,23 @@
+﻿// напигите программу которая принимает на вход трехзначное целое число и выходе
+// показывает сумму первой и последней цифры этого числа
+// 456 => 10
+
+Console.WriteLine("Write number N");
+
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N >= 100 && N <= 999)
+{
+
+int A = N / 100;
+int B = N % 10;
+int result = A + B;
+
+Console.Write(result);
+}
+else {
+    Console.WriteLine("the number is not 3-digit");
+}
+
+// && - и
+// || - или
